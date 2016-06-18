a562eb5633c7c22b58a748500b870dd9733bedc2
suhodolik <A96humutu>
2016-06-18 23:24:00 +0300

    12

--- server.py
+++ server.py
@@ -3,24 +3,38 @@
 import socket
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Server():
+    def __init__(self, port=1404, count=1):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(('', port))
+        self.sock.listen(count)  # количество клиентов
+        pass
 
-sock.bind(('', 1404))
-sock.listen(1)  # количество клиентов
+    def file_send(self, conn):
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                # break
+                print ('finish file transfer!!')
+                return
+            conn.send(data.upper())
 
-conn, addr = sock.accept()
+    def connect(self):
+        conn_socket, addr = self.sock.accept()
+        print ('connected: ', addr)
+        return conn_socket, addr
+
+    def disconnect(self, conn):
+        conn.close()
 
 
-print 'connected: ', addr
+if __name__ == "__main__":
+    my_serv = Server()
+    print (111)
+    connect, adr = my_serv.connect()
+    print (222)
+    my_serv.file_send(connect)
 
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-
-    conn.send(data.upper())
-
-conn.close()
 
 
 
